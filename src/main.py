3d3df656ddf36016968266a35f6da4eee34ca25b
AmirHossein Roodaki <amirhossein.rdk@gmail.com>
2023-02-01 12:49:51 +0330

    Output Compressed-File's Extension Updated to .bin

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ image_path = input("Image Path: ")  # IO/Inputs/Cat.jpg
 image_bit_string = file_handling.read_image_bit_string(image_path)
 compressed_image_bit_string = huffman_coding.compress(image_bit_string)
 file_handling.write_image(compressed_image_bit_string,
-                          "IO/Outputs/compressed_image.jpg")
+                          "IO/Outputs/compressed_image.bin")
 print("Compression Ratio (CR):",
       len(image_bit_string) / len(compressed_image_bit_string))
 decompressed_image_bit_string = huffman_coding.decompress(
